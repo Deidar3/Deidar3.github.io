dbe2b995ecbe4fd21862486d87068fb8b41855cf
deidar <szymonowski3@gmail.com>
2025-07-11 20:06:18 +0200

    Updates

---
+++ static/js/react-syntax-highlighter_languages_refractor_brainfuck.87c17805.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkblog_react=self.webpackChunkblog_react||[]).push([[9101],{2644:e=>{function a(e){e.languages.brainfuck={pointer:{pattern:/<|>/,alias:"keyword"},increment:{pattern:/\+/,alias:"inserted"},decrement:{pattern:/-/,alias:"deleted"},branching:{pattern:/\[|\]/,alias:"important"},operator:/[.,]/,comment:/\S+/}}e.exports=a,a.displayName="brainfuck",a.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_brainfuck.87c17805.chunk.js.map
